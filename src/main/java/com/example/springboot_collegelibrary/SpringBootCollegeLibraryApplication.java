d744d50a5d4fb6b092aafd0b7eae6fa508d8f4d3
ThomasChoi <724thomas@gmail.com>
2022-12-02 15:00:53 +0900

    Initial Commit

---
+++ src/main/java/com/example/springboot_collegelibrary/SpringBootCollegeLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_collegelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCollegeLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCollegeLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_collegelibrary/SpringBootCollegeLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_collegelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCollegeLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
