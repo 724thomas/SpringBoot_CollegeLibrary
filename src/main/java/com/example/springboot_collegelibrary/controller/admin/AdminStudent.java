6a5c569080c0edad71a0240a3a73764e422ec0f1
ThomasChoi <724thomas@gmail.com>
2022-12-12 16:12:51 +0900

    Feat : Admin can see all the book rent, return, point history of a student.

--- src/main/java/com/example/springboot_collegelibrary/controller/admin/AdminStudent.java
+++ src/main/java/com/example/springboot_collegelibrary/controller/admin/AdminStudent.java
@@ -1,8 +1,11 @@
 package com.example.springboot_collegelibrary.controller.admin;
 
 import com.example.springboot_collegelibrary.Service.AdminService;
+import com.example.springboot_collegelibrary.Service.MoneyTransactionService;
+import com.example.springboot_collegelibrary.Service.StudentHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,11 +15,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AdminStudent {
 
     @Autowired
-    private AdminService adminService;
+    AdminService adminService;
+    @Autowired
+    StudentHistoryService studentHistoryService;
+    @Autowired
+    MoneyTransactionService moneyTransactionService;
 
     @GetMapping("/studentDelete")
     public String studentDelete(@RequestParam String studentEmail){
         System.out.println("AdminStudent.studentDelete : Student(" + studentEmail + ") Deleted");
         return "redirect:/admin/studentManagement";
+    }
+
+    @GetMapping("/studentDetail")
+    public String studentDetail(@RequestParam String studentEmail, Model model){
+        model.addAttribute("studentCurrentBorrowDTO",studentHistoryService.getStudentCurrentBorrowList(studentEmail));
+        model.addAttribute("studentHistoryDTO",studentHistoryService.getStudentBorrowedBookHistory(studentEmail));
+        model.addAttribute("totalTransactionDTOList", moneyTransactionService.selectStudentTotalTransactionByEmail(studentEmail));
+        return "adminStudentDetail";
     }
 }
